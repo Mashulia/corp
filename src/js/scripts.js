d578fbf42049fdff90ea8bf204a2450763d118ef
lollikay <lottakayumova@gmail.com>
2021-08-03 17:30:25 +0500

    updates

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -777,6 +777,25 @@ window.addEventListener("load", function () {
   //   e.preventDefault();
   // });
 
+  class splitter {
+    constructor(el) {
+      Object.assign(this, {
+        $wrapper: el
+      });
+      this.$trigger = this.$wrapper.children[0].querySelector(".split__toggle");
+      this.$trigger.onclick = () => this.toggle();
+    }
+
+    toggle() {
+      console.log("click");
+      this.$wrapper.classList.toggle(`split--opened`);
+    }
+  }
+
+  document.querySelectorAll(".split").forEach((el) => {
+    new splitter(el);
+  });
+
   // functions on resize
   window.onresize = function (event) {
     delay(function () {
