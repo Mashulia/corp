0b2430bb4854fb13f0c8f8e8191a643dcc9a274e
MariaR <rudneva@it-tian.ru>
2021-12-24 10:34:09 +0500

    test

--- src/js/cart-vue/components/cart-item.vue
+++ src/js/cart-vue/components/cart-item.vue
@@ -1,115 +1,115 @@
 <template>
-  <transition>
-    <div class="cart-contents__item" ref="animateDeleteItem" v-if="show">
-      <div class="cart-contents__row">
-        <a v-if="product.pic" class="cart-contents__image" :href="product.link">
-          <!-- Размер картинок 124х93-->
-          <img :src="product.pic" />
-        </a>
-        <div v-else class="cart-contents__image no-image"></div>
-        <div class="cart-contents__info">
-          <a class="cart-contents__name" :href="product.link">
-            {{ product.name }}</a
-          >
-          <div class="cart-contents__price">
-            <div v-if="product.price > 0" class="cart-contents__price__value">
-              {{ (product.price * 1).toLocaleString() }} {{ product.currency }}
-            </div>
-            <div v-else class="cart-contents__price__value">
-              {{ this.cartTextData.ON_REQUEST_TEXT }}
-            </div>
-            <div class="cart-contents__price__label">
-              {{ this.cartTextData.PRISE_FOR_UNIT }}
-            </div>
+  <!-- <transition-group name="fade"> -->
+  <div class="cart-contents__item" v-if="show">
+    <div class="cart-contents__row">
+      <a v-if="product.pic" class="cart-contents__image" :href="product.link">
+        <!-- Размер картинок 124х93-->
+        <img :src="product.pic" />
+      </a>
+      <div v-else class="cart-contents__image no-image"></div>
+      <div class="cart-contents__info">
+        <a class="cart-contents__name" :href="product.link">
+          {{ product.name }}</a
+        >
+        <div class="cart-contents__price">
+          <div v-if="product.price > 0" class="cart-contents__price__value">
+            {{ (product.price * 1).toLocaleString() }} {{ product.currency }}
+          </div>
+          <div v-else class="cart-contents__price__value">
+            {{ this.cartTextData.ON_REQUEST_TEXT }}
+          </div>
+          <div class="cart-contents__price__label">
+            {{ this.cartTextData.PRISE_FOR_UNIT }}
           </div>
         </div>
-        <div class="cart-contents__pricing">
-          <div class="cart-contents__qty">
-            <div class="qty">
-              <button
-                class="qty__change qty__change--minus"
-                @click="decrementProduct"
-              >
-                -
-              </button>
-              <input
-                class="qty__input form__input"
-                type="number"
-                v-model="product.qty"
-                min="1"
-                max="99"
-                @blur="validateInputData($event)"
-              />
-              <button
-                class="qty__change qty__change--plus"
-                @click="incrementProduct"
-              >
-                +
-              </button>
-            </div>
-          </div>
-          <div class="cart-contents__price">
-            <div v-if="product.price > 0" class="cart-contents__price__value">
-              <animated-integer
-                :value="multiplication"
-                :product="product"
-              ></animated-integer>
-              {{ product.currency }}
-            </div>
-            <div v-else class="cart-contents__price__value">
-              {{ this.cartTextData.ON_REQUEST_TEXT }}
-            </div>
-            <div class="cart-contents__price__label">
-              x {{ product.qty }} {{ this.cartTextData.UNIT }}
-            </div>
-          </div>
-        </div>
-        <div class="cart-contents__remove">
-          <button
-            class="button-remove button button-icon"
-            title="Удалить товар из корзины"
-            @click="deleteFromCart"
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+      </div>
+      <div class="cart-contents__pricing">
+        <div class="cart-contents__qty">
+          <div class="qty">
+            <button
+              class="qty__change qty__change--minus"
+              @click="decrementProduct"
             >
-              <g id="Icon/Outline/trash-2" opacity="0.6">
+              -
+            </button>
+            <input
+              class="qty__input form__input"
+              type="number"
+              v-model="product.qty"
+              min="1"
+              max="99"
+              @blur="validateInputData($event)"
+            />
+            <button
+              class="qty__change qty__change--plus"
+              @click="incrementProduct"
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <div class="cart-contents__price">
+          <div v-if="product.price > 0" class="cart-contents__price__value">
+            <animated-integer
+              :value="multiplication"
+              :product="product"
+            ></animated-integer>
+            {{ product.currency }}
+          </div>
+          <div v-else class="cart-contents__price__value">
+            {{ this.cartTextData.ON_REQUEST_TEXT }}
+          </div>
+          <div class="cart-contents__price__label">
+            x {{ product.qty }} {{ this.cartTextData.UNIT }}
+          </div>
+        </div>
+      </div>
+      <div class="cart-contents__remove">
+        <button
+          class="button-remove button button-icon"
+          title="Удалить товар из корзины"
+          @click="deleteFromCart"
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="Icon/Outline/trash-2" opacity="0.6">
+              <path
+                id="Mask"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M10 16C10 16.55 9.55 17 9 17C8.45 17 8 16.55 8 16V12C8 11.45 8.45 11 9 11C9.55 11 10 11.45 10 12V16ZM16 16C16 16.55 15.55 17 15 17C14.45 17 14 16.55 14 16V12C14 11.45 14.45 11 15 11C15.55 11 16 11.45 16 12V16ZM18 19C18 19.551 17.552 20 17 20H7C6.448 20 6 19.551 6 19V8H18V19ZM10 4.328C10 4.173 10.214 4 10.5 4H13.5C13.786 4 14 4.173 14 4.328V6H10V4.328ZM21 6H20H16V4.328C16 3.044 14.879 2 13.5 2H10.5C9.121 2 8 3.044 8 4.328V6H4H3C2.45 6 2 6.45 2 7C2 7.55 2.45 8 3 8H4V19C4 20.654 5.346 22 7 22H17C18.654 22 20 20.654 20 19V8H21C21.55 8 22 7.55 22 7C22 6.45 21.55 6 21 6Z"
+                fill="currentColor"
+              ></path>
+              <mask
+                id="mask0"
+                mask-type="alpha"
+                maskUnits="userSpaceOnUse"
+                x="2"
+                y="2"
+                width="20"
+                height="20"
+              >
                 <path
-                  id="Mask"
+                  id="Mask_2"
                   fill-rule="evenodd"
                   clip-rule="evenodd"
                   d="M10 16C10 16.55 9.55 17 9 17C8.45 17 8 16.55 8 16V12C8 11.45 8.45 11 9 11C9.55 11 10 11.45 10 12V16ZM16 16C16 16.55 15.55 17 15 17C14.45 17 14 16.55 14 16V12C14 11.45 14.45 11 15 11C15.55 11 16 11.45 16 12V16ZM18 19C18 19.551 17.552 20 17 20H7C6.448 20 6 19.551 6 19V8H18V19ZM10 4.328C10 4.173 10.214 4 10.5 4H13.5C13.786 4 14 4.173 14 4.328V6H10V4.328ZM21 6H20H16V4.328C16 3.044 14.879 2 13.5 2H10.5C9.121 2 8 3.044 8 4.328V6H4H3C2.45 6 2 6.45 2 7C2 7.55 2.45 8 3 8H4V19C4 20.654 5.346 22 7 22H17C18.654 22 20 20.654 20 19V8H21C21.55 8 22 7.55 22 7C22 6.45 21.55 6 21 6Z"
-                  fill="currentColor"
+                  fill="white"
                 ></path>
-                <mask
-                  id="mask0"
-                  mask-type="alpha"
-                  maskUnits="userSpaceOnUse"
-                  x="2"
-                  y="2"
-                  width="20"
-                  height="20"
-                >
-                  <path
-                    id="Mask_2"
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M10 16C10 16.55 9.55 17 9 17C8.45 17 8 16.55 8 16V12C8 11.45 8.45 11 9 11C9.55 11 10 11.45 10 12V16ZM16 16C16 16.55 15.55 17 15 17C14.45 17 14 16.55 14 16V12C14 11.45 14.45 11 15 11C15.55 11 16 11.45 16 12V16ZM18 19C18 19.551 17.552 20 17 20H7C6.448 20 6 19.551 6 19V8H18V19ZM10 4.328C10 4.173 10.214 4 10.5 4H13.5C13.786 4 14 4.173 14 4.328V6H10V4.328ZM21 6H20H16V4.328C16 3.044 14.879 2 13.5 2H10.5C9.121 2 8 3.044 8 4.328V6H4H3C2.45 6 2 6.45 2 7C2 7.55 2.45 8 3 8H4V19C4 20.654 5.346 22 7 22H17C18.654 22 20 20.654 20 19V8H21C21.55 8 22 7.55 22 7C22 6.45 21.55 6 21 6Z"
-                    fill="white"
-                  ></path>
-                </mask>
-                <g mask="url(#mask0)"></g>
-              </g>
-            </svg>
-          </button>
-        </div>
+              </mask>
+              <g mask="url(#mask0)"></g>
+            </g>
+          </svg>
+        </button>
       </div>
     </div>
-  </transition>
+  </div>
+  <!-- </transition-group> -->
 </template>
 <script>
 import gsap from "gsap";
@@ -120,8 +120,9 @@ export default {
   components: { animatedInteger },
   data() {
     return {
+      tweeningValue: this.multiplication,
       show: true,
-      tweeningValue: this.multiplication
+      cartItem: null
     };
   },
   props: {
@@ -146,6 +147,10 @@ export default {
     ]),
     deleteFromCart() {
       this.$emit("deleteFromCart");
+      // show = !show;
+    },
+    animateDelete() {
+      show = !show;
     },
     incrementProduct() {
       this.$emit("incrementProduct");
@@ -183,6 +188,7 @@ export default {
   },
   mounted() {
     this.tween(this.value, Number(this.multiplication));
+    console.log(this.product.link);
   }
 };
 </script>
